0f2b40c96eb48411cf7e6f1152422c9e3d4db835
Xudaverdi <xudaverdi.sh@gmail.com>
2024-01-06 21:43:13 +0400

    test

---
+++ src/Components/TodoForm.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+
+function TodoForm({ saveTodos, updateTask, edit }) {
+    const [value, setValue] = useState("");
+    const [isEditing, setIsEditing] = useState(false);
+
+    useEffect(() => {
+        if (edit) {
+            setIsEditing(true);
+            setValue(edit.name);
+        } else {
+            setIsEditing(false);
+            setValue("");
+        }
+    }, [edit]);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if (value.trim() === "") return;
+
+        if (isEditing) {
+            updateTask(value);
+            setIsEditing(false);
+        } else {
+            saveTodos(value);
+        }
+
+        setValue("");
+    };
+
+    return (
+        <><h1 className="text-center text-xl font-bold">Your Todo App</h1>
+            <form onSubmit={(e) => onSubmit(e)} className="flex space-x-[10px] items-center">
+                <input
+                    placeholder="Write your task..."
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                    type="text"
+                    className="outline-blue-400 flex-1 border px-4 h-[35px]"
+                />
+                <button
+                    disabled={!value.trim()}
+                    type="submit"
+                    className='disabled:opacity-40 disabled:cursor-not-allowed 
+                         bg-blue-500 rounded p-2 text-white'
+                >
+                    {isEditing ? 'Update' : 'Add'}
+                </button>
+
+            </form>
+        </>
+    );
+}
+
+export default TodoForm;
---
+++ src/Components/TodoItems.jsx
@@ -0,0 +1,47 @@
+import { LuTrash2 } from "react-icons/lu";
+import { CiEdit } from "react-icons/ci";
+import { useState } from 'react';
+
+function TodoItems({ todos, onDelete, onEdit }) {
+    const [checkedItems, setCheckedItems] = useState([]);
+
+    const handleCheckboxChange = (id) => {
+        if (checkedItems.includes(id)) {
+            //remove check
+            setCheckedItems(checkedItems.filter((itemId) => itemId !== id)); //return id
+
+        } else {
+            // add check
+            setCheckedItems([id]);
+
+        }
+
+    };
+
+    return (
+        <div className=' space-y-2'>
+            {todos.map((item, index) =>
+                <div key={index} className={`flex justify-between items-center border p-2 bg-white ${checkedItems.includes(item.id) ? 'line-through' : ''}`}>
+                    <div className="flex items-center">
+                        <input type="checkbox" onChange={() => handleCheckboxChange(item.id)} />
+                        <span className='ml-1'>{item.name}</span>
+                    </div>
+                    <div className="space-x-2">
+                        <button
+                            onClick={() => onEdit(item.id)}
+                            className="bg-green-500 px-3 py-2 rounded text-white inline-flex justify-center items-center">
+                            <CiEdit />
+                        </button>
+                        <button
+                            onClick={() => onDelete(item.id)}
+                            className="bg-red-500 px-3 py-2 rounded text-white inline-flex justify-center items-center">
+                            <LuTrash2 />
+                        </button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default TodoItems;
